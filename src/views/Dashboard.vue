6b1269688c0ee982ceecfd331d880ebee11f6f76
HoraceHyy <horacehuang17@gmail.com>
2023-08-11 15:53:31 +0800

    fix. disable axis ticks for x-axis

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -102,6 +102,9 @@ onMounted(() => {
             },
             axisLabel: {
                 show: false
+            },
+            axisTick: {
+                show: false
             }
         },
         yAxis: {
