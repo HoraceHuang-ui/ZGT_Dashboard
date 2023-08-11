d44350864328b86c69f23072919e4fec9a727862
HoraceHyy <horacehuang17@gmail.com>
2023-08-11 14:26:30 +0800

    feat. add echarts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import naive from 'naive-ui'
+import * as echarts from "echarts"
 
 const app = createApp(App)
 
